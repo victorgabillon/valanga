35f8ee8268f6d03eb83740c68af89e28b6199653
Victor Gabillon <victor.gabillon@thalesgroup.com>
2025-10-06 20:20:11 +0200

    init added for exposure

---
+++ valanga/__init__.py
@@ -0,0 +1,5 @@
+"""Common types and utilities shared by multiple libraries."""
+
+from .evaluations import ForcedOutcome, FloatyBoardEvaluation, BoardEvaluation
+from .over_event import OverEvent
+__all__ = ["ForcedOutcome", "FloatyBoardEvaluation", "BoardEvaluation", "OverEvent"]
