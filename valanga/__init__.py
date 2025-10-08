c09122f2c3efb74f6662c49e699f4afdd6d67d97
Victor Gabillon <victor.gabillon@thalesgroup.com>
2025-10-08 09:35:56 +0200

    exposing HasTurn

--- valanga/__init__.py
+++ valanga/__init__.py
@@ -2,6 +2,6 @@
 
 from .evaluations import ForcedOutcome, FloatyBoardEvaluation, BoardEvaluation
 from .over_event import OverEvent
-from .game import Color, WHITE, BLACK, Colors
+from .game import Color, WHITE, BLACK, Colors, HasTurn
 
-__all__ = ["ForcedOutcome", "FloatyBoardEvaluation", "BoardEvaluation", "OverEvent", "Color", "WHITE", "BLACK", "Colors"]
+__all__ = ["ForcedOutcome", "FloatyBoardEvaluation", "BoardEvaluation", "OverEvent", "Color", "WHITE", "BLACK", "Colors", "HasTurn"]
