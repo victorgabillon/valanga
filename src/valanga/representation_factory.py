298bbabe3f2f3dd192efb728a20d9c1d5e2f28c3
Victor Gabillon <victor.gabillon@thalesgroup.com>
2026-01-25 15:36:32 +0100

    jhand

--- src/valanga/representation_factory.py
+++ src/valanga/representation_factory.py
@@ -6,18 +6,17 @@ from dataclasses import dataclass
 from typing import Protocol
 
 from .game import State
-from .represention_for_evaluation import ContentRepresentation
 
 
-class CreateFromState[StateT: State, EvalIn](Protocol):
+class CreateFromState[StateT: State, RepT](Protocol):
     """
     Protocol for creating a state representation from a state.
     """
 
-    def __call__(self, state: StateT) -> ContentRepresentation[StateT, EvalIn]: ...
+    def __call__(self, state: StateT) -> RepT: ...
 
 
-class CreateFromStateAndModifications[StateT: State, EvalIn, StateModT](Protocol):
+class CreateFromStateAndModifications[StateT: State, StateModT, RepT](Protocol):
     """
     Protocol for creating a state representation from a state and modifications.
     """
@@ -26,29 +25,29 @@ class CreateFromStateAndModifications[StateT: State, EvalIn, StateModT](Protocol
         self,
         state: StateT,
         state_modifications: StateModT,
-        previous_state_representation: ContentRepresentation[StateT, EvalIn],
-    ) -> ContentRepresentation[StateT, EvalIn]: ...
+        previous_state_representation: RepT,
+    ) -> RepT: ...
 
 
 @dataclass
-class RepresentationFactory[StateT: State, EvalIn, StateModT]:
+class RepresentationFactory[StateT: State, StateModT, RepT]:
     """Factory for creating content representations from states and state modifications.
     Attributes:
         create_from_state: Function to create a content representation from a state.
         create_from_state_and_modifications: Function to create a content representation from a state and state modifications.
     """
 
-    create_from_state: CreateFromState[StateT, EvalIn]
+    create_from_state: CreateFromState[StateT, RepT]
     create_from_state_and_modifications: CreateFromStateAndModifications[
-        StateT, EvalIn, StateModT
+        StateT, StateModT, RepT
     ]
 
     def create_from_transition(
         self,
         state: StateT,
-        previous_state_representation: ContentRepresentation[StateT, EvalIn] | None,
+        previous_state_representation: RepT | None,
         modifications: StateModT | None,
-    ) -> ContentRepresentation[StateT, EvalIn]:
+    ) -> RepT:
         """Creates a content representation from a state transition.
         Args:
             state: The current state of the game.
